14e96f2325acc345ee15f0307eeb05ec4390cdc1
Kayro Mizael <kayromsa@gmail.com>
2020-03-13 00:21:45 -0300

    checking await async with csv-parser

--- app.js
+++ app.js
@@ -2,13 +2,33 @@ const csv = require('csv-parser');
 const fs = require('fs');
 
 let dataset1 = [];
+let dataset2 = [];
 
-fs.createReadStream('teste.csv')
-.pipe(csv())
-.on('data', (row) => {
-	dataset1 += row;
-})
-.on('end', () => {
+main();
+
+async function main() {
+	await readFile('dataset1.csv');
+	await readFile('dataset2.csv');
+
 	console.log(dataset1);
-	console.log('CSV file successfully processed');
-});
+console.log(dataset2);
+}
+
+async function readFile(file) {
+	var temp = [];
+	fs.createReadStream(file)
+		.pipe(csv())
+		.on('data', (row) => {
+			if (file.includes('set1')) {
+				dataset1.push(row);
+			}
+			if (file.includes('set2')) {
+				dataset2.push(row);
+			}
+		})
+		.on("end", function () {
+			console.log("We are done!")
+		});
+		
+}
+
